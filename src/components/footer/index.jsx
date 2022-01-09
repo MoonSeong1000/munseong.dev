84758d5cfe182804b6ad7646a82b31a9db28b6be
천문성 <cms9586@naver.com>
2022-01-09 17:12:29 +0900

    ✨ 링크 수정

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -4,9 +4,5 @@ import './index.scss'
 
 export const Footer = () => (
   <footer className="footer">
-    ©<a href="https://github.com/JaeYeopHan">Jbee</a>, Built with{' '}
-    <a href="https://github.com/JaeYeopHan/gatsby-starter-bee">
-      Gatsby-starter-bee
-    </a>
   </footer>
 )
--- src/components/social-share/github-icon/index.jsx
+++ src/components/social-share/github-icon/index.jsx
@@ -5,7 +5,7 @@ import './index.scss'
 export const GitHubIcon = () => {
   return (
     <a
-      href="https://github.com/JaeYeopHan/felog"
+      href="https://github.com/MoonSeong1000"
       className="github"
       aria-label="GitHub"
       target="_blank"
